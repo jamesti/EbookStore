cef7887091bf4d707b9ddc4afe9cb1ccf7ffc617
James <jamesmsjr@gmail.com>
2019-07-17 21:57:35 -0300

    Salvando na Nuvem!

--- app/src/main/java/senac/ebookstore/EbookActivity.java
+++ app/src/main/java/senac/ebookstore/EbookActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+
+import senac.ebookstore.models.Ebook;
 
 public class EbookActivity extends AppCompatActivity {
+
+    TextView txtIsbn, txtTitulo, txtAutor, txtTipo, txtSinopse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +26,29 @@ public class EbookActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        txtIsbn = findViewById(R.id.txtISBN);
+        txtTipo = findViewById(R.id.txtTipo);
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtAutor = findViewById(R.id.txtAutor);
+        txtSinopse = findViewById(R.id.txtSinopse);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                String isbn = txtIsbn.getText().toString();
+                String titulo = txtTitulo.getText().toString();
+                String autor = txtAutor.getText().toString();
+                String tipo = txtTipo.getText().toString();
+                String sinopse = txtSinopse.getText().toString();
+
+                Ebook ebook = new Ebook(isbn, null, titulo, autor, tipo, sinopse, null);
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("ebook-" + ebook.getIsbn());
+
+                myRef.setValue(ebook);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
